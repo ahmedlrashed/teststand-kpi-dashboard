3fda7396edf0f8784087e5ee851cab14886ac9ac
Ahmed L Rashed <arashed07@gmail.com>
2023-08-04 12:32:37 -0400

    Initialize README and config.R

---
+++ 0-config.R
@@ -0,0 +1,7 @@
+#==========================#
+# STEP 0: INSTALL PACKAGES #
+#==========================#
+
+# Execute this script only for first call to initialize environment
+
+install.packages("shiny")
