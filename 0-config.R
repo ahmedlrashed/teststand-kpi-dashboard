bc363ef928da9b0a32c4f54c8a5bf58d2d09976c
Ahmed L Rashed <110828918+ahmedlrashed@users.noreply.github.com>
2024-08-12 11:33:32 -0400

    Delete 0-config.R

--- 0-config.R
+++
@@ -1,7 +0,0 @@
-#==========================#
-# STEP 0: INSTALL PACKAGES #
-#==========================#
-
-# Execute this script only for first call to initialize environment
-
-install.packages("shiny")
